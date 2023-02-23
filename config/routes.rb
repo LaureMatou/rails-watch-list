a2b7e33b3067fe757f864fbdd3401d7117742f69
Laure Matoussowsky <laurematoussowsky@gmail.com>
2023-02-23 18:03:37 +0100

    tests green

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:new, :create, :destroy]
+    resources :bookmarks, only: [:new, :create]
   end
+  resources :bookmarks, only: :destroy
 end
 
 # As a user, I can see all my movie lists
