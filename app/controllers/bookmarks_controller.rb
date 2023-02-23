5137cad871ce8a5df12f5dae10a106889d515347
Laure Matoussowsky <laurematoussowsky@gmail.com>
2023-02-23 18:04:34 +0100

    refacto

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,8 +1,8 @@
 class BookmarksController < ApplicationController
-  def new
-    @bookmark = Bookmark.new
-    @list = List.find(params[:list_id])
-  end
+  # def new
+  #   @bookmark = Bookmark.new
+  #   @list = List.find(params[:list_id])
+  # end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
